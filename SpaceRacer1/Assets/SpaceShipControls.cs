2d1d716ec3deab0f8002fa9cc8ab50af25364899
SamStrelitz <aersixb9@hotmail.com>
2017-03-01 06:28:43 -0800

    Ship Rotates with Mouse
    Ship Rotates with Mouse

---
+++ SpaceRacer1/Assets/SpaceShipControls.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceShipControls : MonoBehaviour {
+
+    float _rotAngle = 0f;
+    float rotSpeed = 5f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float movement = Input.GetAxis("Mouse X");
+        
+        _rotAngle -= movement * rotSpeed * Time.deltaTime;
+
+        this.transform.Rotate(0, 0, _rotAngle);
+        
+	}
+}
