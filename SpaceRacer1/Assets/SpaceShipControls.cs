c206acf85370701e55c9a2a66a1b450378958158
SamStrelitz <aersixb9@hotmail.com>
2017-03-10 16:37:28 -0800

    Working on the thrust

--- SpaceRacer1/Assets/SpaceShipControls.cs
+++ SpaceRacer1/Assets/SpaceShipControls.cs
@@ -8,7 +8,7 @@ public class SpaceShipControls : MonoBehaviour {
     float rotSpeed = 1.4f;
     Vector3 _velocity;
  //   Vector3 _position;
-    float _thrust = 1f;
+    float _thrust = 100000f;
 	// Use this for initialization
 	void Start () {
         _velocity = new Vector3(0f, 0f, 0f);
@@ -20,23 +20,37 @@ public class SpaceShipControls : MonoBehaviour {
 	void Update () {
 
         float movement = Input.GetAxis("Horizontal");
-        
+        bool thrusting = false;
         _rotAngle -= movement * rotSpeed * Time.deltaTime;
 
         this.transform.Rotate(0, 0, _rotAngle);
 
-        /*float vert = Input.GetAxis("Vertical");
+        float vert = Input.GetAxis("Vertical");
         if (vert < 0)
             vert = 0;
+        if (vert > 0)
+        {
+            thrusting = true;
+      //      Debug.Log("Thrusting");
+        }
 
-        Vector3 UnitForward = transform.forward;
-        _velocity.x += UnitForward.x * _thrust * vert;
-        _velocity.y += UnitForward.y * _thrust * vert;
+        if (thrusting)
+        {
+            Vector3 UnitForward = transform.forward;
+            Debug.Log("X: " + UnitForward.x + " Y:" + UnitForward.y);
+            //make a unit vector in the forward direction
+          //  UnitForward = UnitForward / UnitForward.magnitude;
 
-        _position.x += _velocity.x;
-        _position.y += _velocity.y;*/
+            _velocity.x += UnitForward.x * _thrust * vert * Time.deltaTime;
+            _velocity.y += UnitForward.y * _thrust * vert * Time.deltaTime; 
+            _velocity.z = 0f;
 
-        _velocity.y = .02f; // * Time.deltaTime;
+        }
+
+        //_position.x += _velocity.x;
+        //_position.y += _velocity.y;*/
+
+        //_velocity.y = .02f; // * Time.deltaTime;
         transform.Translate(_velocity);
 
     }
