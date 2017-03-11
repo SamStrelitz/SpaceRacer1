24a21c8bceb681ef0e136e04b5bdea56e4636517
SamStrelitz <aersixb9@hotmail.com>
2017-03-10 18:12:26 -0800

    Now with working thrust, but broken rotation.

--- SpaceRacer1/Assets/SpaceShipControls.cs
+++ SpaceRacer1/Assets/SpaceShipControls.cs
@@ -8,7 +8,7 @@ public class SpaceShipControls : MonoBehaviour {
     float rotSpeed = 1.4f;
     float _gRot = 0;
     Vector3 _velocity;
-    float _thrust = 1f;
+    float _thrust = .1f;
 
     void Start () {
         _velocity = new Vector3(0f, 0f, 0f);
@@ -34,12 +34,15 @@ public class SpaceShipControls : MonoBehaviour {
 
         if (thrusting)
         {
-            Vector3 UnitForward = new Vector3(0, 0, 0);
-            Debug.Log("LocalRotX: " + this.transform.localRotation.x);
-            UnitForward.x = Mathf.Sin(_gRot);
-            UnitForward.y = Mathf.Cos(_gRot);
-            UnitForward = Vector3.ClampMagnitude(UnitForward, 1);
-            Debug.Log("gRot:" + _gRot + "X: " + UnitForward.x + " Y:" + UnitForward.y);
+            /*    Vector3 UnitForward = new Vector3(0, 0, 0);
+                Debug.Log("LocalRotX: " + this.transform.localRotation.x);
+                UnitForward.x = Mathf.Sin(_gRot);
+                UnitForward.y = Mathf.Cos(_gRot);
+                UnitForward = Vector3.ClampMagnitude(UnitForward, 1);
+                Debug.Log("gRot:" + _gRot + "X: " + UnitForward.x + " Y:" + UnitForward.y); */
+
+            Debug.Log("Transform.right: X: " + transform.right.x + " Y:" + transform.right.y);
+            Vector3 UnitForward = transform.up;
       
             _velocity.x += UnitForward.x * _thrust * vert * Time.deltaTime;
             _velocity.y += UnitForward.y * _thrust * vert * Time.deltaTime; 
