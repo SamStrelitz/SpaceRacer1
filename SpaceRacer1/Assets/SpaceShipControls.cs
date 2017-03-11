565a70f520ef2f2f3fcf7cea6438b81eedfa0800
SamStrelitz <aersixb9@hotmail.com>
2017-03-10 17:43:06 -0800

    Still working on rotation/thrust

--- SpaceRacer1/Assets/SpaceShipControls.cs
+++ SpaceRacer1/Assets/SpaceShipControls.cs
@@ -6,17 +6,15 @@ public class SpaceShipControls : MonoBehaviour {
 
     float _rotAngle = 0f;
     float rotSpeed = 1.4f;
+    float _gRot = 0;
     Vector3 _velocity;
- //   Vector3 _position;
-    float _thrust = 100000f;
-	// Use this for initialization
-	void Start () {
+    float _thrust = 1f;
+
+    void Start () {
         _velocity = new Vector3(0f, 0f, 0f);
- //       _position = new Vector3(0f, 0f, 0f);
 
 	}
 	
-	// Update is called once per frame
 	void Update () {
 
         float movement = Input.GetAxis("Horizontal");
@@ -24,33 +22,30 @@ public class SpaceShipControls : MonoBehaviour {
         _rotAngle -= movement * rotSpeed * Time.deltaTime;
 
         this.transform.Rotate(0, 0, _rotAngle);
+        _gRot -= _rotAngle;
 
         float vert = Input.GetAxis("Vertical");
         if (vert < 0)
             vert = 0;
-        if (vert > 0)
+        else if (vert > 0)
         {
             thrusting = true;
-      //      Debug.Log("Thrusting");
         }
 
         if (thrusting)
         {
-            Vector3 UnitForward = transform.forward;
-            Debug.Log("X: " + UnitForward.x + " Y:" + UnitForward.y);
-            //make a unit vector in the forward direction
-          //  UnitForward = UnitForward / UnitForward.magnitude;
-
+            Vector3 UnitForward = new Vector3(0, 0, 0);
+            Debug.Log("LocalRotX: " + this.transform.localRotation.x);
+            UnitForward.x = Mathf.Sin(_gRot);
+            UnitForward.y = Mathf.Cos(_gRot);
+            UnitForward = Vector3.ClampMagnitude(UnitForward, 1);
+            Debug.Log("gRot:" + _gRot + "X: " + UnitForward.x + " Y:" + UnitForward.y);
+      
             _velocity.x += UnitForward.x * _thrust * vert * Time.deltaTime;
             _velocity.y += UnitForward.y * _thrust * vert * Time.deltaTime; 
             _velocity.z = 0f;
 
         }
-
-        //_position.x += _velocity.x;
-        //_position.y += _velocity.y;*/
-
-        //_velocity.y = .02f; // * Time.deltaTime;
         transform.Translate(_velocity);
 
     }
